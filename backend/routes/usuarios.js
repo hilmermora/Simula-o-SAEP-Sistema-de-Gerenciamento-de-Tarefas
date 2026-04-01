3967ab263b866ec50f79788f2c1f394a5b4fcf13
hilmer_mora <hilmer_gutierrez@estudante.sesisenai.org.br>
2026-04-01 13:43:09 -0300

    .

--- backend/routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/usuarios');
-
-router.get('/', controller.listarUsuarios);
-router.post('/', controller.cadastrarUsuario);
-
-module.exports = router;
